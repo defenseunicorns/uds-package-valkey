8dd6cf28df67f0ad0bc24a7947e890b15657bbe8
Joe Richardson <joe@defenseunicorns.com>
2024-11-21 13:29:10 -0500

    wip

--- tests/watch-jobs.sh
+++ tests/watch-jobs.sh
@@ -2,6 +2,8 @@
 # Copyright 2024 Defense Unicorns
 # SPDX-License-Identifier: AGPL-3.0-or-later OR LicenseRef-Defense-Unicorns-Commercial
 
+set -e
+
 # List of job names to monitor (space-separated string)
 JOBS="valkey-test-standalone valkey-test-replicated-w-sentinel"
 NAMESPACE="valkey-test"
@@ -19,13 +21,13 @@ is_job_complete() {
 }
 
 # Monitoring loop
-while [ -n "${JOBS}" ]; do
+while [[ -n "${JOBS}" ]]; do
   UNFINISHED_JOBS=""
 
   for job in ${JOBS}; do
     # Check for failed condition
     job_status="$(get_job_status "${job}")"
-    if [ "${job_status}" = "True" ]; then
+    if [[ "${job_status}" = "True" ]]; then
       echo "Error: Job '${job}' failed."
       kubectl describe -n "${NAMESPACE}" jobs.batch "${job}"
       kubectl describe namespace "${NAMESPACE}"
@@ -35,7 +37,7 @@ while [ -n "${JOBS}" ]; do
 
     # Check if the job is complete
     job_complete="$(is_job_complete "${job}")"
-    if [ "${job_complete}" = "True" ]; then
+    if [[ "${job_complete}" = "True" ]]; then
       echo "Job '${job}' completed successfully."
     else
       # Add unfinished job back to the list
@@ -47,9 +49,8 @@ while [ -n "${JOBS}" ]; do
   JOBS="${UNFINISHED_JOBS}"
 
   # Break the loop if all jobs are completed
-  if [ -z "${JOBS}" ]; then
+  if [[ -z "${JOBS}" ]]; then
     echo "All jobs completed successfully."
-    break
   fi
 
   # Wait before the next check
